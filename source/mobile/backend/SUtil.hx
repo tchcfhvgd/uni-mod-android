eaa57a7690fc83b4d516b0b449ac1f1ab577d77a
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-07-21 14:26:07 +0800

    Add files via upload

---
+++ source/mobile/backend/SUtil.hx
@@ -0,0 +1,106 @@
+package mobile.backend;
+
+import lime.system.System as LimeSystem;
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+
+#if sys
+import sys.*;
+import sys.io.*;
+#elseif js
+import js.html.*;
+#end
+
+using StringTools;
+
+/**
+ * A storage class for mobile.
+ * @author Mihai Alexandru (M.A. Jigsaw) and Lily (mcagabe19)
+ */
+class SUtil
+{
+	public static function mkDirs(directory:String):Void
+	{
+		var total:String = '';
+		if (directory.substr(0, 1) == '/')
+			total = '/';
+
+		var parts:Array<String> = directory.split('/');
+		if (parts.length > 0 && parts[0].indexOf(':') > -1)
+			parts.shift();
+
+		for (part in parts)
+		{
+			if (part != '.' && part != '')
+			{
+				if (total != '' && total != '/')
+					total += '/';
+
+				total += part;
+
+				try
+				{
+					if (!FileSystem.exists(total))
+						FileSystem.createDirectory(total);
+				}
+				catch (e:haxe.Exception)
+					trace('Error while creating folder. (${e.message}');
+			}
+		}
+	}
+
+	public static function saveContent(fileName:String = 'file', fileExtension:String = '.json',
+			fileData:String = 'You forgor to add somethin\' in yo code :3'):Void
+	{
+		try
+		{
+			if (!FileSystem.exists('saves'))
+				FileSystem.createDirectory('saves');
+
+			File.saveContent('saves/' + fileName + fileExtension, fileData);
+			showPopUp(fileName + " file has been saved.", "Success!");
+		}
+		catch (e:haxe.Exception)
+			trace('File couldn\'t be saved. (${e.message})');
+	}
+
+	#if android
+	public static function checkExternalPaths(?splitStorage = false):Array<String> {
+		var process = new Process('grep -o "/storage/....-...." /proc/mounts | paste -sd \',\'');
+		var paths:String = process.stdout.readAll().toString();
+		if (splitStorage) paths = paths.replace('/storage/', '');
+		return paths.split(',');
+	}
+
+	public static function getExternalDirectory(external:String):String {
+		var daPath:String = '';
+		for (path in checkExternalPaths())
+			if (path.contains(external)) daPath = path;
+
+		daPath = haxe.io.Path.addTrailingSlash(daPath.endsWith("\n") ? daPath.substr(0, daPath.length - 1) : daPath);
+		return daPath;
+	}
+	#end
+	
+	public static function showPopUp(message:String, title:String):Void
+	{
+		#if (!ios || !iphonesim)
+		try
+		{
+			trace('$title - $message');
+			lime.app.Application.current.window.alert(message, title);
+		}
+		catch (e:Dynamic)
+			trace('$title - $message');
+		#else
+		trace('$title - $message');
+		#end
+	}
+}
